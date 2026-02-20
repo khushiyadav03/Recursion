df43aaef163735b869c7f75b1067e967bd46c9e8
Khushi Yadav <ky0307005@gmail.com>
2026-02-20 22:43:15 +0530

    Time: 3 ms (21.49%), Space: 9.4 MB (34.67%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        vector<string> parts;
+        int count = 0, start = 0;
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '1') count++;
+            else count--;
+            
+            // Found a balanced substring
+            if (count == 0) {
+                // Recursively solve inside
+                string inner = s.substr(start + 1, i - start - 1);
+                parts.push_back("1" + makeLargestSpecial(inner) + "0");
+                start = i + 1;
+            }
+        }
+        
+        // Sort in descending lexicographic order
+        sort(parts.begin(), parts.end(), greater<string>());
+        
+        string result = "";
+        for (auto &p : parts) {
+            result += p;
+        }
+        
+        return result;
+    }
+};
